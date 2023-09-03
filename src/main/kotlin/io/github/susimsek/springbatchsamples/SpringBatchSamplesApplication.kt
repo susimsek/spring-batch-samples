c86ebfdd507d3bb63e8329196fc8554089134c94
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-09-03 21:33:34 +0300

    refactor main class

--- src/main/kotlin/io/github/susimsek/springbatchsamples/SpringBatchSamplesApplication.kt
+++ src/main/kotlin/io/github/susimsek/springbatchsamples/SpringBatchSamplesApplication.kt
@@ -1,11 +1,18 @@
 package io.github.susimsek.springbatchsamples
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
+
 
 @SpringBootApplication
 class SpringBatchSamplesApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringBatchSamplesApplication>(*args)
+    exitProcess(
+        SpringApplication.exit(
+            runApplication<SpringBatchSamplesApplication>(*args)
+        )
+    )
 }
