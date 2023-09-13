4ba9e527668e05f5990a2f257e0dcd3e058f3a56
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-09-13 21:54:11 +0300

    refactor cache config

--- src/main/kotlin/io/github/susimsek/springbatchsamples/config/CacheConfig.kt
+++ src/main/kotlin/io/github/susimsek/springbatchsamples/config/CacheConfig.kt
@@ -7,6 +7,7 @@ import org.redisson.Redisson
 import org.redisson.api.RedissonClient
 import org.redisson.codec.SnappyCodecV2
 import org.redisson.config.Config
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -54,8 +55,8 @@ class CacheConfig {
         return Redisson.create(config)
     }
 
-
     @Bean
+    @ConditionalOnProperty("spring.jpa.properties.hibernate.cache.use_second_level_cache")
     fun hibernatePropertiesCustomizer(redissonClient: RedissonClient): HibernatePropertiesCustomizer {
         return HibernatePropertiesCustomizer { hibernateProperties ->
             hibernateProperties[AvailableSettings.CACHE_REGION_FACTORY] = SpringRedissonRegionFactory(redissonClient)
